f1e987b9067ff65509c9dfcce212cd153c9eeb30
gafurzy <gafurzy64@gmail.com>
2025-09-21 17:42:19 +0700

    ZC.Safe Calls

--- src/ZC.Safe Calls.kt
+++ src/ZC.Safe Calls.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 //Safe calling
 //?.let
-    var maybeNumber: Int? = 15
+    var maybeNumber: Int? = 16
     maybeNumber?.let { println(it) }
 //The ?. chaining
 //student?.teacher?.supervisor?.name = "Seymour Skinner"
